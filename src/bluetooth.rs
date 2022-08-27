55a67fb05c166905260c7b87003046ff4e9fdcaf
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2022-08-27 02:59:17 -0400

    bluetooth testing (#2)
    * blue
    
    * bluetooth func
    
    * fixes
    
    Co-authored-by: Daniel Liu <mr.picklepinosaur@gmail.com>

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -1,1 +1,63 @@
+use std::{collections::HashSet, env, thread, time::Duration};
 
+use anyhow::Result;
+use bluer::{Adapter, AdapterEvent, Address, Device, DeviceEvent};
+use futures::{pin_mut, stream::SelectAll, StreamExt};
+
+async fn query_device(adapter: &Adapter, addr: Address) -> Result<()> {
+    let device = adapter.device(addr)?;
+    println!("    Address type:       {}", device.address_type().await?);
+    println!("    Name:               {:?}", device.name().await?);
+    println!("    Icon:               {:?}", device.icon().await?);
+    println!("    Class:              {:?}", device.class().await?);
+    println!(
+        "    UUIDs:              {:?}",
+        device.uuids().await?.unwrap_or_default()
+    );
+    println!("    Paried:             {:?}", device.is_paired().await?);
+    println!("    Connected:          {:?}", device.is_connected().await?);
+    println!("    Trusted:            {:?}", device.is_trusted().await?);
+    println!("    Modalias:           {:?}", device.modalias().await?);
+    println!("    RSSI:               {:?}", device.rssi().await?);
+    println!("    TX power:           {:?}", device.tx_power().await?);
+    println!(
+        "    Manufacturer data:  {:?}",
+        device.manufacturer_data().await?
+    );
+    println!("    Service data:       {:?}", device.service_data().await?);
+    Ok(())
+}
+
+pub async fn rssi_by_inquiry() -> Result<String> {
+    let session = bluer::Session::new().await?;
+    let adapter = session.default_adapter().await?;
+    println!(
+        "Discovering devices using Bluetooth adapater {}\n",
+        adapter.name()
+    );
+    adapter.set_powered(true).await.unwrap();
+    println!("powered on");
+
+    let device_events = adapter.discover_devices().await?;
+
+    println!("Scanning for bluetooth addresses");
+    thread::sleep(Duration::from_secs(10));
+
+    let mut addrs = adapter.device_addresses().await.unwrap();
+    let mut named_addrs: Vec<(String, i16)> = Vec::new();
+
+    for addr in addrs.iter() {
+        let device = adapter.device(addr.clone())?;
+        match device.name().await? {
+            Some(x) => {
+                println!("{}", x.clone());
+                println!("{}", device.rssi().await?.unwrap());
+                named_addrs.push((x, device.rssi().await?.unwrap()));
+            },
+            None => (),
+        }
+    }
+    named_addrs.sort_by(|(n1, r1), (n2, r2)| r2.cmp(r1));
+
+    return Ok(named_addrs[0].0.clone());
+}
--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,40 @@ mod pi_gpio;
 use std::{sync::mpsc, thread};
 
 use anyhow::Result;
+use bluetooth::rssi_by_inquiry;
 use models::Item;
 use pino_utils::ok_or_continue_msg;
 
 use crate::camera::take_picture;
 
-fn main() {
-    // let image = take_picture("/dev/video2").unwrap();
-    // println!("{:?}", image);
-    // classify("nithin", image).unwrap();
-    // gpio_test().unwrap();
+#[tokio::main]
+async fn main() {
     let (motion_tx, motion_rx) = mpsc::channel::<Vec<u8>>();
     let (gpio_tx, gpio_rx) = mpsc::channel::<Item>();
 
     let opencv_handle = thread::spawn(move || {
         motion::opencv_test(motion_tx, 2).unwrap();
     });
-    let api_handle = thread::spawn(move || {
-        for recv in motion_rx {
-            println!("recieved");
-            let resp = ok_or_continue_msg!(api::classify("nithin", recv), |e| {
-                // println!("{:?}", e);
-            });
-            if gpio_tx.send(resp.item_type).is_err() {
-                println!("Error sending to gpio thread");
-            }
-        }
-    });
     let gpio_handle = thread::spawn(move || {
         for recv in gpio_rx {
             println!("motor for item {:?}", recv);
         }
     });
+
+    for recv in motion_rx {
+        println!("recieved");
+        let device_name = ok_or_continue_msg!(rssi_by_inquiry().await, |e| {
+            println!("{:?}", e);
+        });
+        /*
+        let resp = ok_or_continue_msg!(api::classify("nithin", recv), |e| {
+            // println!("{:?}", e);
+        });
+        if gpio_tx.send(resp.item_type).is_err() {
+            println!("Error sending to gpio thread");
+        }
+        */
+    }
 
     opencv_handle.join().unwrap();
 }
