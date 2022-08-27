010eb46b8691933971c85dda4c03109bad889732
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-27 13:22:18 -0400

    Audio playback (#5)
    * Audio playback
    
    * small error handle;

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@
 #![allow(dead_code)]
 
 mod api;
+mod audio;
 mod bluetooth;
 mod camera;
 mod models;
 mod motion;
 mod pi_gpio;
 
-use std::{sync::mpsc, thread};
+use std::{
+    sync::mpsc,
+    thread::{self, JoinHandle},
+};
 
 use anyhow::Result;
+use audio::play_audio;
 use bluetooth::rssi_by_inquiry;
 use models::Item;
 use pi_gpio::gpio_test;
@@ -20,24 +25,32 @@ use crate::camera::take_picture;
 
 #[tokio::main]
 async fn main() {
+    // intialize channels
     let (motion_tx, motion_rx) = mpsc::channel::<Vec<u8>>();
     let (gpio_tx, gpio_rx) = mpsc::channel::<Item>();
 
+    // motion detection thread
     let opencv_handle = thread::spawn(move || {
-        motion::opencv_test(motion_tx, 2).unwrap();
+        if let Err(e) = motion::opencv_test(motion_tx, 2) {
+            println!("{:?}", e);
+        }
     });
+
+    // gpio thread
     let gpio_handle = thread::spawn(move || {
         for recv in gpio_rx {
             println!("motor for item {:?}", recv);
         }
     });
 
+    // main thread handles bluetooth discovery
+    // might not need handles to each audio thread
+    let mut audio_thread_pool: Vec<JoinHandle<()>> = Vec::new();
     for recv in motion_rx {
-        println!("recieved");
+        /*
         let device_name = ok_or_continue_msg!(rssi_by_inquiry().await, |e| {
             println!("{:?}", e);
         });
-        /*
         let resp = ok_or_continue_msg!(api::classify("nithin", recv), |e| {
             // println!("{:?}", e);
         });
@@ -45,6 +58,12 @@ async fn main() {
             println!("Error sending to gpio thread");
         }
         */
+        let audio_handle = thread::spawn(move || {
+            if let Err(e) = play_audio() {
+                println!("Error playing audio {:?}", e);
+            }
+        });
+        audio_thread_pool.push(audio_handle);
     }
 
     opencv_handle.join().unwrap();
